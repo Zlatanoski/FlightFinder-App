efe133cf088428ae9200fb325861c491bd0b8364
zlatanoski <zlatanoskidavid92@gmail.com>
2025-08-27 00:37:47 +0200

    Add requirements.txt and prepare for deployment

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def index():
     return render_template("index.html", flights=flight_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
